809ad828d31e270abd8e1b7c6b3e3be011d46075
Kate Summerill <kaitlynsummerill@gmail.com>
2019-10-21 02:31:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  display_board = ["  |  ", "  |  ", "  |  "]
+  top_row = ["   ", "   ", "   "]
+  split_one = ""
 end
 
 print display_board
