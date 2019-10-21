60ac44f515e1882670364b8fc75ec6a66104b2c3
Kate Summerill <kaitlynsummerill@gmail.com>
2019-10-21 02:56:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,7 +10,7 @@
 def display_board
 # board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
 #board output
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts " #{board[]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
